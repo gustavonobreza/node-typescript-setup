efbd46eccded1bb69be3bf10cea6292e22c0aa70
Gustavo Alexandre Nobre Mesquita <gnobremesquita@gmail.com>
2021-05-13 04:16:09 -0300

    ✅Just missing some middwares in Express to be perfect

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { user } from '@controllers/index'
-import { User } from '@models/User'
-console.log(user instanceof User)
+import { app } from './app'
+import http from 'http'
+
+http.createServer(app).listen(3400)
+console.log('Server running in: http://localhost:3400')
